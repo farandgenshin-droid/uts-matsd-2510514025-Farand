024a59c842bc97b816039d7c79ee15c7a417334d
farandgenshin-droid <farandgenshin@gmail.com>
2026-04-22 09:40:50 +0700

    coba tes

---
+++ 02_matlab/soal_2_invers_citra.m
@@ -0,0 +1,8 @@
+% ===================================================== 
+%  UTS Matematika Sains Data — Semester Genap 2025/2026 
+%  Soal 2 — Enkripsi Citra Logo Toko dengan Invers Matriks 3x3
+%  Nama  : Muhammad Farand Danendra
+%  NIM   : 2510514025 
+%  Parameter: N=25, a=2, b=5, K=8, theta0=85, alpha=0.006 
+%  Tanggal: 22 April 2026 
+% =====================================================
